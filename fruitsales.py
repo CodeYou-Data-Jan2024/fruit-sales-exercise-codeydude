e04efa6e9deddee7590b5405c8de37ba5ba40346
Lavin Lalchandani <lavinl27@gmail.com>
2024-05-02 18:33:00 -0400

    update

--- fruitsales.py
+++ fruitsales.py
@@ -1,8 +1,6 @@
 # add your code 
 import pandas as pd
 
-# import pandas as pd
-
 fruit = pd.fruit({"Apples": ["35", "41"],
                   "Bananas": ["21", "34"]},
                   index = ["2017 Sales", "2018 Sales"])

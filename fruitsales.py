57c8ac28a3baeea003694ee40c76541349742438
Lavin Lalchandani <lavinl27@gmail.com>
2024-05-02 18:26:43 -0400

    new

--- fruitsales.py
+++ fruitsales.py
@@ -1,9 +1,12 @@
-# add your code here
-import pandas
+# add your code 
+import pandas as pd
 
-fruit({"Apples": ["35", "41"],
-       "Bananas": ["21", "34"]},
-       index=["2017 Sales", "2018 Sales"])
+# import pandas as pd
+
+fruit = pd.fruit({"Apples": ["35", "41"],
+                  "Bananas": ["21", "34"]},
+                  index = ["2017 Sales", "2018 Sales"])
+print(fruit)
 
 fruit.to_csv("fruit.csv")
 
